ea4c081408c7190a03fbd94ebe499493f765c7a4
Gabriel Paiva Rega <gabrielrega@gmail.com>
2018-02-28 16:05:04 -0300

    Update leitor.R
    Arquivo mudado para ser a forma sugerida de se carregar os dados do arquivo para o R.

--- leitor.R
+++ leitor.R
@@ -1,12 +1,7 @@
 library(dplyr)
-library(tidytext)
-library(lexiconPT)
+library(lubridate)
 
-data("oplexicon_v3.0")
-get_word_sentiment("temer")
-
-bcb <- read_delim("bcb.txt", "\t", escape_double = FALSE, 
-                  col_names = FALSE, trim_ws = TRUE)
-
-
-bcb %>% rename(text = X1) %>% unnest_tokens(term, text) %>% inner_join(oplexicon_v3.0) %>% .$polarity %>% sum()
+atas <- read.table("atas.txt", header = TRUE) %>%
+  as_tibble() %>% 
+  mutate(data = ymd(data)) %>% 
+  mutate(texto = as.character(texto))
