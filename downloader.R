245c6fcce0e07d39dcc5bd85aff3d599db6b6ab2
Gabriel Paiva Rega <gabrielrega@gmail.com>
2018-02-27 21:02:31 -0300

    DOWNLOAD COMPLETO
    Função download pronta e funcional.
    Arquivos na pasta "atas"

---
+++ downloader.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+download_ata <- function(n_ata = 200) {
+  
+  url = paste0("http://www.bcb.gov.br/?COPOM", n_ata)
+  
+  if (n_ata < 200) {
+    destfile = paste0("./atas/copom_", n_ata, ".html")
+    } else {
+      destfile = paste0("./atas/copom_", n_ata, ".pdf")
+    }
+  
+  download.file(url = url,
+                destfile = destfile,
+                mode = "wb",
+                extra = '-L')
+  
+  
+}
+
+
+seq(from = 21, to = 212) %>% map(download_ata)
